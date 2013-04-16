f48d3b8080cc2b1445d392f8f159b93a21a5da7a
Adam Bray <adam.bray@yale.edu>
2013-04-16 15:19:42 -0400

    added migrate command

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -15,7 +15,7 @@ alias shifts='cd ~/code/stc/shifts && subl .'
 alias resv='cd ~/code/stc/reservations && subl .'
 
 alias dep_res_all='cap deploy DOMAIN=ulua.its.yale.edu PREFIX=bmec BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=forestry BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=stc-loaners BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=bmec-training BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=dmca BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=uof BRANCH=master && cap deploy DOMAIN=ulua.its.yale.edu PREFIX=ycc_bikes BRANCH=master'
-
+alias dep_res_all_migrate='cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=bmec BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=forestry BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=stc-loaners BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=bmec-training BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=dmca BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=uof BRANCH=master && cap deploy:migrate DOMAIN=ulua.its.yale.edu PREFIX=ycc_bikes BRANCH=master'
 alias dep_shifts_all='cap deploy DOMAIN=weke.its.yale.edu PREFIX=apps2 BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=acr BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=fsc BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=mediaservices BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=ssrs BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=stcdev BRANCH=master && cap deploy DOMAIN=weke.its.yale.edu PREFIX=stc_training BRANCH=master'
 
 alias ctags='/usr/local/bin/ctags'
