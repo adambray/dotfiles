bac2ecf3661eb6f008868507c0514ab115298a25
Adam Bray <adam.bray@yale.edu>
2014-02-21 14:38:56 -0500

    diff for opendiff

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -18,6 +18,8 @@ alias dl='cd ~/Downloads'
 alias doc='cd ~/Documents'
 alias dsk='cd ~/Desktop'
 
+alias diff='opendiff'
+
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
 
 alias dotfiles='cd ~/.dotfiles && subl .'
