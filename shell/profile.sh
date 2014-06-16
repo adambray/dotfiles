42449529548c384e8dece5a1e6d093d6d5a0d958
Adam Bray <adam.bray@yale.edu>
2014-06-16 09:09:24 -0400

    removed setting that conflicts with ruby 2.1

--- shell/profile.sh
+++ shell/profile.sh
@@ -82,7 +82,7 @@ fi
 
 # improve performance of rails apps (at cost of memory usage)
 export RUBY_GC_MALLOC_LIMIT=60000000
-export RUBY_FREE_MIN=200000
+# export RUBY_FREE_MIN=200000
 
 # autocomplete ssh servers
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" scp sftp ssh
