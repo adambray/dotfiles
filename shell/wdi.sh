c3d10943338c0558eb00291b3c93a6298c2b292a
Adam Bray <adam.bray@yale.edu>
2014-07-02 11:29:53 -0400

    update wdi scripts

--- shell/wdi.sh
+++ shell/wdi.sh
@@ -2,24 +2,32 @@
 
 # A function that jumps to a particular wk/day folder in the class repo
 
-export WDI_PATH=$CODE_PATH/general_assembly/wdi/
+export WDI_PATH="$CODE_PATH/general_assembly/wdi"
 
-# Students can add a shortcut to quickly access their GA folder
-# example: alias wdi="cd ~/Documents/GA/WDI4"
+tender="$WDI_PATH/map_feb_2014/tender_touches"
+hamco="$WDI_PATH/closure_jun_2014/hamco"
+students="$WDI_PATH/closure_jun_2014/students"
 
 alias wdi="cd $WDI_PATH"
-alias tender="cd $WDI_PATH/map_feb_2014/tender_touches"
-alias hamco="cd $WDI_PATH/closure_jun_2014/hamco"
-alias students="cd $WDI_PATH/closure_jun_2014/students"
+alias tender="cd $tender"
+alias hamco="cd $hamco"
+alias students="cd $students"
 
 cdhwfunc() {
   # takes three args: the week, the day
   # and an optional third for the user
   if [ $1 -lt 10 ]
   then
-    cd ~/code/general_assembly/wdi/WDI_NYC_12_Students/w0$1/d0$2/${3:-Instructor}
+    cd $students/w0$1/d0$2/${3:-INSTRUCTOR}*
   else
-    cd ~/dev/wdi/WDI_NYC_12_Students/w$1/d0$2/${3:-Instructor}
+    cd $students/w$1/d0$2/${3:-INSTRUCTOR}*
   fi
 }
-alias cdhw=cdhwfun
+alias cdhw=cdhwfunc
+
+cpr() {
+  # Checks out a Pull Request
+  # usage: cpr pull_request_id
+  git fetch origin
+  git checkout pr/$1
+}
